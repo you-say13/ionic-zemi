9d34641651e253c3d855e6a6cf9a85e762e21795
Okawa ECC <98862325+you-say13@users.noreply.github.com>
2023-01-02 15:17:23 +0900

    アラート用ファイルの作成

---
+++ src/views/components/conformation-alert.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {
+    alertController
+} from '@ionic/vue'
+
+export default defineComponent({
+    setup() {
+        const conf_alert = async (head:string, shead:string, ms:string, b_yes:string, b_no:string) =>{
+            const alert = await alertController.create({
+                header: head,
+                subHeader: shead,
+                message: ms,
+                buttons: [
+                    {
+                        text: b_yes,
+                        role:"confirm",
+                        handler: () =>{
+                            alertflag.value = true
+                        }
+                    },
+                    {
+                        text: b_no,
+                        role:"cancel",
+                        handler: ()=>{
+                            alertflag.value = false
+                        }
+                    }
+                    
+                ]
+            })
+            
+            await alert.present();
+        }
+
+        return { conf_alert };
+    },
+    components:{
+        alertController
+    }
+})
+</script>
