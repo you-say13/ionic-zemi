478a7054e403c3a3dc03936ca52fdbb264acb65c
Okawa ECC <nukohitu@gmail.com>
2023-01-10 16:24:00 +0900

    alertの追加とファイルの整理

---
+++ src/views/comp/fetch_component.ts
@@ -0,0 +1,109 @@
+export const fetch_component = () =>{
+    const allfetch = async (ipaddress: string, auth_info: number, desc: string) =>{
+        const url = "http://"+ipaddress+"/zemi/select"
+        let tododata;
+        const data = {
+            user_id : auth_info,
+            desc : desc
+        }
+        await fetch(url, {
+            method:"POST",
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+        .then(response => {
+            return response.json()
+        }).then(res =>{
+            if(res.message == "error"){
+                alert("アクセス回数が多すぎます。少し時間をおいて下さい")
+            }else{
+                tododata = res
+            }
+        })
+        .catch((error)=>{
+            console.log("occurred error:" + error)
+        })
+        return await tododata
+    }
+
+    //その２：削除
+    const del = async (ipaddress: string, auth_info:number, todo_id: number) =>{
+        const addr:string = "http://"+ipaddress+"/zemi/delete"
+        const data = {
+            todo_id : todo_id,
+            user_id : auth_info
+        }
+        await fetch(addr, {
+            method:"POST",
+            headers:{
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response=>{
+            return response
+        })
+    }
+
+    //その３：更新 
+    const upd = async (ipaddress: string, todo_id: number, flag:boolean) =>{
+        const addr = "http://"+ipaddress+"/zemi/update"
+        const data = {
+            todo_id : todo_id,
+            flag : flag,
+        }
+        await fetch(addr, {
+            method:"POST",
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+        .then(response=>{
+            return response
+        }).then(res=>{
+            console.log(res)
+            alert("TODOの達成状況を変更しました")
+        }).catch(err=>{
+            console.log(err)
+            alert("達成に変更できませんでした")
+        })
+    }
+
+    const progress = async(ipaddress: string, auth_info: number, desc: string, flag : number) =>{
+        let prog_todo;
+        console.log(ipaddress+":"+auth_info+":"+desc+":"+flag)
+        const addr:string = "http://"+ipaddress+"/zemi/prog"
+        const data = {
+            flag : flag,
+            userid : auth_info,
+            desc : desc
+        }
+        await fetch(addr, {
+            method:"POST",
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+        .then((response)=> {
+            return response.json()
+        })
+        .then((res)=>{
+            prog_todo = res
+            console.log()
+        })
+        .catch((err)=>console.error(err))
+
+        return await prog_todo
+    }
+
+    return{
+        allfetch,
+        del,
+        upd,
+        progress,
+    }
+}
